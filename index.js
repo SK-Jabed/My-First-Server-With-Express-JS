b0ca0afcdafb7469999a0bea11ee5f1984562387
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-22 23:42:46 +0600

    Server Site Creates for Client Site

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+const phones = require("./phones.json");
 const app = express();
 const port = 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello from my first ever server");
@@ -8,6 +12,17 @@ app.get("/", (req, res) => {
 
 app.get("/data", (req, res) => {
     res.send("More data will be added soon")
+})
+
+app.get("/phones", (req, res) => {
+    res.send(phones);
+})
+
+app.get("/phones/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log("I need data for id: ", id);
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phone);
 })
 
 app.listen(port, () => {
